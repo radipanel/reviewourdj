5dcb7f7d6a5e053758be08377a5e82be77d45853
Matthew Gall <matthew@matthew-ubuntu.(none)>
2010-07-13 21:15:03 +0100

    Finished script

--- _res/mgmt/viewreviews.php
+++ _res/mgmt/viewreviews.php
@@ -1,3 +1,111 @@
-<?php
+<form action="" method="post" id="viewReviews">
 
-?>
+	<div class="box">
+
+		<?php
+
+			// Check if a user has been selected
+			if ( $_POST['submit'] ) {
+
+				// First we get their ID from the POST submission
+				$id = $_POST['username'];
+
+				// Then we turn that into a username!
+				$username = $db->query( "SELECT username FROM users WHERE id='{$id}'" );
+				$username = mysql_result($username,"username");
+
+			?>
+			
+				<div class="square title">
+					<strong>DJ Reviews for <?php echo $username; ?></strong>
+				</div>
+
+				<div class="content">
+					<?php
+					// Time to get their reviews!
+					$reviews = $db->query( "SELECT * FROM reviews WHERE dj='{$id}'" );
+					$num = $db->num( $reviews );
+
+					$j = "a";
+
+					// And now to format them ;)
+					while( $array = $db->assoc( $reviews ) ) {
+
+						echo "<div class=\"row {$j}\" id=\"review_{$array['id']}\">";
+						echo "<strong>From:</strong> " . $array['habbo'];
+						echo "<br />";
+						echo "<strong>Received:</strong> " . date("d.m.Y", $array['time']);
+						echo "<br />";
+						echo "<strong>IP:</strong> " . $array['ip'];
+						echo "<br />";
+						echo "<strong>Review:</strong> " . $array['review'];					
+						echo "</div>";
+
+						$j++;
+
+						if( $j == "c" ) {			
+							
+								$j = "a";
+			
+						}
+
+					}
+
+					if ($num == "0" ) {
+
+						echo "<div class=\"square bad\" style=\"margin-bottom: 0px;\">";
+						echo "<strong>Sorry</strong>";
+						echo "<br />";
+						echo "No reviews have been submitted for this user.";
+						echo "</div>";
+
+					}
+
+					?>
+				</div>
+			<?php
+			}
+			else {
+
+		?>
+				<div class="square title">
+					<strong>View DJ Reviews</strong>
+				</div>
+
+				<div class="content">
+					<p>Select a user from the drop down list below to view any reviews made for that user.</p>
+					
+					<table width="100%" cellpadding="3" cellspacing="0">
+
+					<?php
+					// First, we grab all the users
+					$getUsers = $db->query( "SELECT * FROM users" );
+
+					// Then use a while loop to create the array with it's values
+					while( $array = $db->assoc( $getUsers ) ) {
+					
+							$users[$array['id']] = $array['username'];
+						
+					}
+	
+					echo $core->buildField( "select",
+													"required",
+													"username",
+													"Username",
+													"Whose reviews shall we look at?",
+													$users);
+			
+					?>
+	
+					</table>
+
+				<div class="box" align="right">
+					<input class="button" type="submit" name="submit" value="Submit" />
+				</div>
+
+				<?php
+					echo $core->buildFormJS('viewReviews');
+				}
+		?>
+	</div>
+
